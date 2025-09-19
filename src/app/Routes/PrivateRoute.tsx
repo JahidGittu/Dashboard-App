f3797f0694756e2e29ef3e1cd9d6ca52ecf2fb41
Jahid Hossen <jahid.hossen.me@gmail.com>
2025-09-19 12:01:27 +0600

    Private Routes

---
+++ src/app/Routes/PrivateRoute.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { ReactNode, useEffect } from "react";
+import { LoadingSpinner } from "../dashboard/Components/Loading";
+
+interface PrivateRouteProps {
+  children: ReactNode;
+}
+
+export default function PrivateRoute({ children }: PrivateRouteProps) {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.replace("/auth/page"); // redirect to sign-in page
+    }
+  }, [status, router]);
+
+  if (status === "loading") {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (status === "unauthenticated") return null;
+
+  return <>{children}</>;
+}
